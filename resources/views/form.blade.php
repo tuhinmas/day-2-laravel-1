e14ceea9f29ec2557821e0830e24bba04bb8c2a4
tuhinmas <mastuhin33@gmail.com>
2020-06-30 23:15:17 +0700

    upload dulu

---
+++ resources/views/form.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<head>
+    <title>
+        Form
+    </title>
+</head>
+<body>
+    <h1>
+        Buat Account Baru!
+    </h1>
+    <h3>
+        Sign Up Form
+    </h3>
+    <form method = "POST" action="/welcome">
+        <p>First Name:</p>
+        <input name="nama" size=60>
+
+        <p>Last Name:</p>
+        <input name="nama" size=60>
+
+        <p>Gender:</p>
+        <input type="radio" name="sex">male<br>
+        <input type="radio" name="sex">Female<br>
+        <input type="radio" name="sex">Other
+
+        <p>Nationality:</p>
+        <select name="nationality" id="nationality">
+            <option value="Indonesian">Indonesian</option>
+            <option value="English">English</option>
+            <option value="Other">Other</option>
+        </select>
+
+        <p>Language Spoken:</p>
+        <input type="radio" name="indonesia">Bahasa Indonesia<br>
+        <input type="radio" name="english">English<br>
+        <input type="radio" name="other">Other<br>
+
+        <p>Bio:</p>
+        <textarea cols="60" rows="7">
+        </textarea>
+
+        <br>
+        <input type="submit" value="Sin Up">
+        </button>
+    </form>
+
+</body>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head>
+    <title>
+        sanber book
+    </title>
+</head>
+<body>
+    <h1>
+        SanberBook
+    </h1>
+    <h2>
+        Social Media Developer Santai Berkualitas
+    </h2>
+    <p>
+        Belajar dan Berbagi agar Hidup ini Semakin Santai Berkualitas
+    </p>
+    <h3>
+        Benefit Join di SanberBook
+    </h3>
+    <ul>
+        <li>Mendapatkan motivasi dari sesama developer</li>
+        <li>Sharing knowledge dari para mastah Sanber</li>
+        <li>Dibuat oleh calon web developer terbaik</li>
+    </ul>
+    <h3>
+        Cara Bergabung ke SanberBook
+    </h3>
+    <ol>
+        <li>Mengunjungi Website ini</li>
+        <li>Mendaftar di <a href="/register">Form Sign Up</a></li>
+        <li>Selesai</li>
+    </ol>
+</body>
